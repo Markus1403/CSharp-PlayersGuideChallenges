f4c208e12ecaa81c79909b0d7acedb2a543097d7
Markus1403 <markus15b@gmail.com>
2025-06-05 13:58:09 +0200

    Solutions to the challenges for Records and Generics

---
+++ Records/Program.cs
@@ -0,0 +1,13 @@
+﻿Sword BasicSword = new Sword(MaterialType.iron, GemstoneType.None, 0, 0);
+var BasicGreatsword = BasicSword with { length = 100, crossguardWith = 30 };
+var BasicSmallsword = BasicSword with { length = 80, crossguardWith = 15 };
+Console.WriteLine($"Basic Greatsword: {BasicGreatsword}");
+Console.WriteLine($"Basic Smallsword: {BasicSmallsword}");
+
+public record Sword(MaterialType matetrial, GemstoneType Gemstone, int length, int crossguardWith);
+
+
+public enum MaterialType { wood, bronze, iron, steel, binarium }
+
+
+public enum GemstoneType { emerald, amber, sapphire, diamond, bitstone, None}
